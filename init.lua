4ff77ab04337ca5e4c7ba59effb98b463e68a976
DoctorSulla <matthew.halliday@gmail.com>
2024-01-25 07:11:01 +0000

    Added jj shortcut

--- init.lua
+++ init.lua
@@ -625,6 +625,10 @@ vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = 'Toggle location list' })
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = 'Toggle lsp references' })
 
+-- Custom Keybindings
+map('i', 'jj', '<Esc>', opts)
+
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
